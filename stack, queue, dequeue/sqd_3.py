ef26d7b53a78d26734637a57059243b8912e0e92
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-01-23 16:06:59 +0900

    sqd_3.py fail commit

--- stack, queue, dequeue/sqd_3.py
+++ stack, queue, dequeue/sqd_3.py
@@ -9,9 +9,15 @@ for i in range(N):
     array.append(num)
 
 for i in range(N):
-    if i == 0:
-        for _ in range(array[i]):
+    for j in range(N):
+        if i == 0:
             print("+")
+            j = N - array[i] + 1
         print("-")
-    else:
-        if(array[i] > array[i+1])
+        if i == N-1:
+            a
+        elif i > 0 and i < N-1:
+            if array[i-1] > array[i]:
+                if array[i-1] - array[i] == 1:
+                    print("-")
+    
