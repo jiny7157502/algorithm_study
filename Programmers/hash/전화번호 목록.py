f66a92aeacdf6c178efe9f87824eddea3d5d9e54
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-03-07 14:34:26 +0900

    hash_2번 commit

--- Programmers/hash/전화번호 목록.py
+++ Programmers/hash/전화번호 목록.py
@@ -1,7 +1,10 @@
 def solution(phone_book):
-    dic = {}
-    phone_book
-    answer = True
-    return answer
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+        else:
+            return True
 
 print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
