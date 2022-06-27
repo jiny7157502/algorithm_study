37de043f55464a9b5866b2840cb5e3106d5131ad
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-06-27 17:41:44 +0900

    더 맵게.py commit(효율성 에러)

--- Programmers/Heap/더 맵게.py
+++ Programmers/Heap/더 맵게.py
@@ -1,24 +1,20 @@
-# def solution(scoville, k):
-#     count = 0
-#     scoville.sort()
-#     while scoville[0] < k:
-#         if len(scoville) == 1:
-#             count = -1
-#             break
-#         else:
-#             new = scoville[0] + scoville[1] * 2
-#             scoville.append(new)
-#             scoville.remove(scoville[0])
-#             scoville.remove(scoville[0])
-#             scoville.sort()
-#             count += 1
+def solution(scoville, k):
+    count = 0
+    scoville.sort()
+    while scoville[0] < k:
+        if len(scoville) == 1:
+            count = -1
+            break
+        else:
+            new = scoville[0] + scoville[1] * 2
+            scoville.append(new)
+            scoville.remove(scoville[0])
+            scoville.remove(scoville[0])
+            scoville.sort()
+            count += 1
 
-#     return count
+    return count
 
-# print(solution([1,2,3,9,10,12], 7)) # 2
-# print(solution([1,1,1,1,1], 7))
-# print(solution([1,2,3,9,10,12], 7))
-
-import heapq
-
-heapq.heapify()
+print(solution([1,2,3,9,10,12], 7)) # 2
+print(solution([1,1,1,1,1], 7))
+print(solution([1,2,3,9,10,12], 7))
