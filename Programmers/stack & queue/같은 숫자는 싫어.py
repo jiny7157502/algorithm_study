07f24486fb596b38989008584d058df1e3f85498
jini <jini@test.com>
2023-01-27 22:10:15 +0900

    stack Q1 commit

--- Programmers/stack & queue/같은 숫자는 싫어.py
+++ Programmers/stack & queue/같은 숫자는 싫어.py
@@ -1,19 +1,15 @@
 def solution(arr):
     result = []
-    status = "yes"
-    for i in range(len(arr)-1):
-        if(status == "yes"):
+    for i in range(len(arr)):
+        if i == 0:
             result.append(arr[i])
-            if(arr[i] == arr[i+1]):
-                status = "no"
-        else:
-            if(arr[i] != arr[i+1]):
-                status = "yes"
-    print(result)
+        elif(arr[i] != arr[i-1]):
+            result.append(arr[i])
+    return result
         
 arr = [1,1,3,3,0,1,1]
 arr2 = [4,4,4,3,3]
 arr3 = [0,0,0,1,0,0,3,4,4,4]
-solution(arr)
-solution(arr2)
-solution(arr3)
+print(solution(arr))
+print(solution(arr2))
+print(solution(arr3))
---
+++ Programmers/stack & queue/올바른 괄호.py
@@ -0,0 +1,3 @@
+def solution(result):
+    return result
+
