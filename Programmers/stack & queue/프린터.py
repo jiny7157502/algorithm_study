13ee6f72da4616a998713b2b1c8f05e78e193428
jini <jiny7157502@naver.com>
2023-01-31 22:50:11 +0900

    printer commit

--- Programmers/stack & queue/프린터.py
+++ Programmers/stack & queue/프린터.py
@@ -1,4 +1,4 @@
-''' def solution(prior, loc):
+def solution(prior, loc):
     max_idx = prior.index(max(prior))
     
     
@@ -7,7 +7,7 @@
     return result
 
 print(solution([2,1,3,2], 2)) # 1
-print(solution([1,1,9,1,1,1], 0)) # 1 '''
+print(solution([1,1,9,1,1,1], 0)) # 1
 
 arr = [2,1,3,2]
 dic = {i : int for i, int in enumerate(arr)}
