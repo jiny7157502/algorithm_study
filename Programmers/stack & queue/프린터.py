7c1bbbc5dc48ac8d3cc3b7965913585e670c75dc
jini <jiny7157502@naver.com>
2023-02-01 23:46:36 +0900

    printer.py commit

--- Programmers/stack & queue/프린터.py
+++ Programmers/stack & queue/프린터.py
@@ -1,16 +1,20 @@
-def solution(prior, loc):
-    max_idx = prior.index(max(prior))
+from collections import deque
+
+def solution(priorities, location):
+    result = 0
     
+    deq = deque([(v, i) for i, v in enumerate(priorities)]) # [(2, 0), (1, 1), (3, 2), (2, 3)]
     
-    while (len(prior) > 0):
-        prior[i] 
+    while len(deq):
+        item = deq.popleft()
+        
+        if deq and item[0] < max(deq)[0]:
+            deq.append(item)
+        else:
+            result += 1
+            if location == item[1]:
+                break
     return result
 
-print(solution([2,1,3,2], 2)) # 1
-print(solution([1,1,9,1,1,1], 0)) # 1
-
-arr = [2,1,3,2]
-dic = {i : int for i, int in enumerate(arr)}
-print(dic)
-
-max_idx = arr.index(max(arr))
+print(solution([2, 1, 3, 2], 2))  # 1
+print(solution([1, 1, 9, 1, 1, 1], 0))  # 1
